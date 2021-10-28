8bdb3bf6e4d524108d287340edc2fb9689d26856
charlesmariga <charlesmariga37@gmail.com>
2021-10-28 12:04:42 +0300

    Refactored fraction module code

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,7 +1,6 @@
 import icons from 'url:../../img/icons.svg';
-import { Fraction } from 'fractional';
-
 import View from './View';
+import Fraction from 'fractional';
 
 class RecipeView extends View {
   addHandlerRender(handler) {
@@ -130,7 +129,7 @@ class RecipeView extends View {
             <use href="${icons}#icon-check"></use>
           </svg>
           <div class="recipe__quantity">${
-            ing.quantity ? new Fraction(ing.quantity).toString() : ''
+            ing.quantity ? new Fraction.Fraction(ing.quantity).toString() : ''
           }</div>
           <div class="recipe__description">
             <span class="recipe__unit">${ing.unit}</span>
